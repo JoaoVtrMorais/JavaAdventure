ee38bd0c84f69149d56b87dc8d03f257896fd381
João Vitor <103777061+JoaoVtrMorais@users.noreply.github.com>
2023-10-03 12:31:27 -0300

    Inserindo fonte personalizada

--- src/main/java/main/UI.java
+++ src/main/java/main/UI.java
@@ -1,29 +1,35 @@
 package main;
 
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Graphics2D;
+import java.awt.*;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.IllegalFormatCodePointException;
 
 public class UI {
     GamePanel gp;
     Graphics2D g2;
-    Font arial_40, arial_80;
+    Font maruMonica;
     public int commandNum = 0;
     public int titleScreenState = 0; // 0: primeira tela, 1: segunda tela
 
     public UI(GamePanel gp) {
         this.gp = gp;
-        
-        arial_40 = new Font("Arial", Font.PLAIN, 40);
-        arial_80 = new Font("Arial", Font.PLAIN, 80);
+
+            try {
+                InputStream is = getClass().getResourceAsStream("/font/x12y16pxMaruMonica.ttf");
+                maruMonica = Font.createFont(Font.TRUETYPE_FONT, is);
+            } catch (FontFormatException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
     }
     
     public void draw(Graphics2D g2) {
 
         this.g2 = g2;
 
-        g2.setFont(arial_80);
+        g2.setFont(maruMonica);
         g2.setColor(Color.white);
 
         // TITLE STATE
